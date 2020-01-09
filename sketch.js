b6fd73d4e26f0d24602cbb7fe1d28096854e4ecf
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2020-01-09 15:56:06 +0100

    definitivo fino a visualizza richiesta

--- sketch.js
+++ sketch.js
@@ -12,16 +12,45 @@ function setup() {
 
   var page2 = select('#page2');
   page2.mousePressed(goToPage3);
+    var arrow2 = select('#arrow2');
+    arrow2.mousePressed(goToPage3);
 
   var page3 = select('#page3');
   page3.mousePressed(goToPage4);
+    var arrow3 = select('#arrow3');
+    arrow3.mousePressed(goToPage4);
 
   var page4 = select('#page4');
   page4.mousePressed(goToPage5);
+    var arrow4 = select('#arrow4');
+    arrow4.mousePressed(goToPage5);
+
+  var button5 = select('#button5');
+  button5.mousePressed(goToPage1);
 
 }
 
 function draw() {
+
+}
+
+function goToPage1() {
+
+  //AGGIUNGERE CLASS ELIMINATED ALLA PAGINA PRIMA!
+  var page4 = select('#page4');
+  page4.removeClass('centered');
+  page4.addClass('eliminated_slideup');
+
+  //eliminare page1
+  var page5 = select('#page5');
+  page5.removeClass('centered_slideup');
+  page5.addClass('eliminated');
+
+  //compare page2
+  var page1 = select('#page1');
+  page1.removeClass('eliminated');
+  page1.addClass('centered');
+
 
 }
 
@@ -37,10 +66,16 @@ function goToPage2() {
   page2.removeClass('eliminated');
   page2.addClass('centered');
 
-    //compare arrow1
-    var arrow1 = select('#arrow1');
-    arrow1.removeClass('eliminated_arrow');
-    arrow1.addClass('centered_arrow');
+    //compare arrow2
+    var arrow2 = select('#arrow2');
+    arrow2.removeClass('eliminated_arrow');
+    arrow2.addClass('centered_arrow');
+
+    var quadratino_1 = select('#quadratino_1');
+    quadratino_1.addClass('white');
+
+    var quadratino_4 = select('#quadratino_4');
+    quadratino_4.removeClass('white');
 
 
 }
@@ -58,6 +93,16 @@ function goToPage3() {
   page3.removeClass('eliminated');
   page3.addClass('centered_slideup');
 
+  //eliminare arrow2
+  var arrow2 = select('#arrow2');
+  arrow2.removeClass('centered_arrow');
+  arrow2.addClass('eliminated_arrow');
+
+  //compare arrow3
+  var arrow3 = select('#arrow3');
+  arrow3.removeClass('eliminated_arrow');
+  arrow3.addClass('centered_arrow');
+
   //quadratino 1 si decolora
   var quadratino_1 = select('#quadratino_1');
   quadratino_1.removeClass('white');
@@ -72,6 +117,7 @@ function goToPage4() {
 
   //AGGIUNGERE CLASS ELIMINATED ALLA PAGINA PRIMA!
   var page2 = select('#page2');
+  page2.removeClass('eliminated_slideup');
   page2.addClass('eliminated');
 
   //spostare page3
@@ -83,6 +129,16 @@ function goToPage4() {
   var page4 = select('#page4');
   page4.removeClass('eliminated');
   page4.addClass('centered_slideup');
+
+  //eliminare arrow3
+  var arrow3 = select('#arrow3');
+  arrow3.removeClass('centered_arrow');
+  arrow3.addClass('eliminated_arrow');
+
+  //compare arrow4
+  var arrow4 = select('#arrow4');
+  arrow4.removeClass('eliminated_arrow');
+  arrow4.addClass('centered_arrow');
 
   //quadratino 2 si decolora
   var quadratino_2 = select('#quadratino_2');
@@ -99,6 +155,7 @@ function goToPage5() {
 
   //AGGIUNGERE CLASS ELIMINATED ALLA PAGINA PRIMA!
   var page3 = select('#page3');
+  page3.removeClass('eliminated_slideup');
   page3.addClass('eliminated');
 
   //spostare page4
@@ -111,6 +168,12 @@ function goToPage5() {
   page5.removeClass('eliminated');
   page5.addClass('centered_slideup');
 
+  //sparisce arrow1
+  var arrow4 = select('#arrow4');
+  arrow4.addClass('eliminated_arrow');
+  arrow4.removeClass('centered_arrow');
+
+
   //quadratino 3 si decolora
   var quadratino_3 = select('#quadratino_3');
   quadratino_3.removeClass('white');
@@ -119,10 +182,6 @@ function goToPage5() {
   var quadratino_4 = select('#quadratino_4');
   quadratino_4.addClass('white');
 
-  //sparisce arrow1
-  var arrow1 = select('#arrow1');
-  arrow1.addClass('eliminated_arrow');
-  arrow1.removeClass('centered_arrow');
 
 
 }
